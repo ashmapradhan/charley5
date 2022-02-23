1464553054bb6b08bdbc77a7decc7200f4a09ad2
Ashma Pradhan <ashmapradhan@gmail.com>
2022-02-22 22:58:05 -0500

    migrate mysql

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\FortifyServiceProvider;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -42,7 +43,7 @@ return new class extends Migration
             $table->dropColumn([
                 'two_factor_secret',
                 'two_factor_recovery_codes',
-            ] + Fortify::confirmsTwoFactorAuthentication() ? [
+            ] + FortifyServiceProvider::confirmsTwoFactorAuthentication() ? [
                 'two_factor_confirmed_at',
             ] : []);
         });
