d6736c838dd5b3019c82ed6aa7d19483aaa46ca6
c-matlock <cmatlock@gmu.edu>
2022-03-30 19:36:31 -0400

    initial code commit, everything works other than email notifications...

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Query\Builder;
+
+class PostsController extends Controller
+{
+
+    public function home() {
+    return view('Jobs.dashboard');
+    }
+
+    public function insertform() {
+        return view('Jobs.index');
+     }
+    public function insert(Request $request){
+        if(!empty($_POST['Job_Name']) && !empty($_POST['Employee']) && !empty($_POST['Description']) && !empty($_POST['Materials']) && !empty($_POST['Start_Date']) && !empty($_POST['End_Date'])){
+        $Job_Name = $request->input('Job_Name');
+        $Employee = $request->input('Employee');
+        $Description = $request->input('Description');
+        $Materials = $request->input('Materials');
+        $Start_Date = $request->input('Start_Date');
+        $End_Date = $request->input('End_Date');
+        $Status = "Requested";
+        
+        if($Start_Date>$End_Date){
+            return view('Jobs.incorrectdates');
+        }
+        
+        $data=array('Job_Name'=>$Job_Name,"Employee"=>$Employee,"Description"=>$Description,"Materials"=>$Materials, "Start_Date"=>$Start_Date, "End_Date"=>$End_Date, "Status"=>"$Status");
+        DB::table('newjobform')->insert($data);
+        return view('Jobs.submissioncomplete');
+        }
+        else{
+            return view('Jobs.submissionerror');
+        }
+    }
+    public function edit(){
+            
+            if(isset($_POST['activate'])){
+                $Id = ($_POST['activate']);
+                $val= array('Status'=>'Active');
+                DB::table('newjobform')->where('Submission_Id', $Id)->update($val);
+                return view('Jobs.activatecomp');
+            }
+            
+            elseif(isset($_POST['delete'])){
+                $Id = ($_POST['delete']);
+                DB::table('newjobform')->where('Submission_Id', $Id)->delete();
+                return view('Jobs.selectioncomplete');
+            }
+        //}
+        else{
+            return view('Jobs.selectionerror');
+        }
+    }
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\GetController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/workRequest', 'GetController@emplData');
+
+Route::post('/workRequest', 'GetController@emplData');
+
+Route::get('/currentJobs', 'GetController@curWrk');
+
+Route::post('/currentJobs', 'GetController@curWrk');
+
+Route::get('insert', 'PostsController@insertform');
+
+Route::post('create', 'PostsController@insert');
+
+Route::post('edit', 'PostsController@edit');
+
+Route::post('/dashboard', 'PostsController@home');
 
 
-Route::get('/home', function () {
-    dd(\Illuminate\Support\Facades\Auth::user());
-})->middleware(['auth', 'verified']);
+
+
