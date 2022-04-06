2784a1e040439fe945bc1da70d7a6a95b813feb3
stacy <stacy.chieng@gmail.com>
2022-04-06 14:37:27 -0400

    Layout template for the website.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+    <div class="content-page">
+        <div class="content">
+            @yield('content')
+        </div>
+    </div>
+    </body>
+</html>
